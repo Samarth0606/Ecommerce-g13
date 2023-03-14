4af634dac82d8ebd207c72c7a0bce524d28a707a
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-14 11:04:22 +0530

    new product added

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ let path = require('path');
 const mongoose = require('mongoose');
 let seedDB = require('./seed');
 let productRoutes = require('./routes/products/productsRoutes')
-
+let ejsMate = require('ejs-mate');
 mongoose.set('strictQuery' , true);
 mongoose.connect('mongodb://127.0.0.1:27017/shopping-g13-app')
 .then(()=>{console.log("DB connected")})
 .catch((err)=>{console.log(err)})
 
 
+app.engine('ejs' , ejsMate);
 app.set('views' , path.join(__dirname , 'views'));
 app.set('view engine' , 'ejs');
+app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname , 'public'))); //static files
 app.use(productRoutes);
 // seedDB();
--- routes/products/productsRoutes.js
+++ routes/products/productsRoutes.js
@@ -2,9 +2,22 @@ let express = require('express');
 const Product = require('../../models/product');
 let router = express.Router();
 
+// display all the products
 router.get('/products' , async(req,res)=>{
     let products = await Product.find();
     res.render('product/index' , {products} )
+})
+
+// to get the form to add new producrt
+router.get('/products/new' , (req,res)=>{
+    res.render('product/new');
+})
+
+// toactually show a product in dbv
+router.post('/products' , async(req,res)=>{
+    let {name , img, price , desc} = req.body;
+    await Product.create({name , img, price , desc});
+    res.redirect('/products');
 })
 
 module.exports = router;
