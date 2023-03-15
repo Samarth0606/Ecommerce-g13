1a9d62443801357b65816ea1ceeb1f7ad9310f64
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-15 10:22:21 +0530

    delte left

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const mongoose = require('mongoose');
 let seedDB = require('./seed');
 let productRoutes = require('./routes/products/productsRoutes')
 let ejsMate = require('ejs-mate');
+var methodOverride = require('method-override');
+
+
+
+
+
 mongoose.set('strictQuery' , true);
 mongoose.connect('mongodb://127.0.0.1:27017/shopping-g13-app')
 .then(()=>{console.log("DB connected")})
 .catch((err)=>{console.log(err)})
 
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 app.engine('ejs' , ejsMate);
 app.set('views' , path.join(__dirname , 'views'));
@@ -17,6 +26,8 @@ app.set('view engine' , 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname , 'public'))); //static files
 app.use(productRoutes);
+
+
 // seedDB();
 
 
--- routes/products/productsRoutes.js
+++ routes/products/productsRoutes.js
@@ -20,4 +20,26 @@ router.post('/products' , async(req,res)=>{
     res.redirect('/products');
 })
 
+// to display a particular product
+router.get('/products/:id' , async(req,res)=>{
+    let {id} =  req.params;
+    let foundProduct = await Product.findById(id);
+    res.render('product/show' , {foundProduct})
+})
+
+// to display an edit form
+router.get('/products/:id/edit' , async(req,res)=>{
+    let {id} =  req.params;
+    let foundProduct = await Product.findById(id);
+    res.render('product/edit' , {foundProduct});
+})
+
+// to actually chnegthe poduct in db
+router.patch('/products/:id' , async (req,res)=>{
+    let {id} =  req.params;
+    let {name , price, img , desc} = req.body;
+    await Product.findByIdAndUpdate(id , {name , price, img , desc} )
+    res.redirect('/products');
+})
+
 module.exports = router;
